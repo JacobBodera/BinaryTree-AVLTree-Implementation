9931ea4969ad66e6dc8d4caea5ca4ddbef3b93f0
Jacob <jbodera@uwaterloo.ca>
2021-07-10 18:07:52 -0400

    Completed some of the functions

--- binary-search-tree.cpp
+++ binary-search-tree.cpp
@@ -7,9 +7,11 @@ typedef BinarySearchTree::Node Node;
 
 using namespace std;
 
-BinarySearchTree::Node::Node(DataType newval)
+BinarySearchTree::Node::Node(DataType newval) // good
 {
-    
+    val = NULL;
+    left = NULL;
+    right = NULL;
 }
 
 int BinarySearchTree::getNodeDepth(Node* n) const
@@ -17,29 +19,46 @@ int BinarySearchTree::getNodeDepth(Node* n) const
     
 }
 
-BinarySearchTree::BinarySearchTree()
+BinarySearchTree::BinarySearchTree() // good
 {
-  
+    root_ = NULL;
+    size_ = 0;
 }
 
-BinarySearchTree::~BinarySearchTree()
+BinarySearchTree::~BinarySearchTree() // good
 {
-    
+
 }
 
-unsigned int BinarySearchTree::size() const
+unsigned int BinarySearchTree::size() const // good
 {
-    
+    return size_;
 }
 
-DataType BinarySearchTree::max() const
+DataType BinarySearchTree::max() const // good
 {
-    
+    Node* current = root_;
+    DataType max = 0;
+
+    while (current)
+    {
+        max = current->val;
+        current = current->right;
+    }
+    return max;
 }
 
-DataType BinarySearchTree::min() const
+DataType BinarySearchTree::min() const // good
 {
-    
+    Node* current = root_;
+    DataType min = 0;
+
+    while (current)
+    {
+        min = current->val;
+        current = current->left;
+    }
+    return min;
 }
 
 unsigned int BinarySearchTree::depth() const
@@ -54,22 +73,58 @@ void BinarySearchTree::print() const
 
 bool BinarySearchTree::exists(DataType val) const
 {
-    
+    if (root_ == NULL)
+        return false;
+
+    Node* current = root_;
+
+    for (int i = 0; i <= depth(); i++)
+    {
+        if (val == current->val)
+            return true;
+        else if (val < current->val)
+        {
+            if (current->left == NULL)
+                return false;
+            current = current->left;
+        }
+        else {
+            if (current->right == NULL)
+                return false;
+            current = current->right;
+        }
+    }
 }
 
-Node* BinarySearchTree::getRootNode()
+Node* BinarySearchTree::getRootNode() // good
 {
-    
+    return root_;
 }
 
-Node** BinarySearchTree::getRootNodeAddress()
+Node** BinarySearchTree::getRootNodeAddress() // good
 {
-    
+    return &root_;
 }
 
 bool BinarySearchTree::insert(DataType val)
 {
-    
+    if (root_ = NULL) {
+        root_->val = val;
+        return true;
+    }
+
+    Node* current = root_;
+
+    while (current) {
+        if (val == current->val)
+            return false;
+        else if (val < current->val)
+            current = current->left;
+        else
+            current = current->right;
+    }
+    current->val = val;
+    return true;
 }
 
 Node* findPredecessor(Node* ptr) {
